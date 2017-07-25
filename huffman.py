3841c6eeae06a0a6a288749b9b3ee7edf6d6324c
Rohan Varma <rvarm1@gmail.com>
2017-07-25 04:17:14 -0700

    first commit

---
+++ huffman.py
@@ -0,0 +1,194 @@
+from collections import defaultdict
+import sys
+import os
+from heapq import heappush, heappop
+from functools import total_ordering
+import pickle
+import binascii # used to convert between hex and binary.
+USAGE_STR = "Usage: python3 huffman.py [test] [encode FILENAME] [decode FILENAME]"
+
+@total_ordering
+class TreeNode(object):
+    # each one has a (str, freq) tuple
+    def __init__(self, letter, num):
+        self.letter = str(letter)
+        self.freq = num
+        self.left = None
+        self.right = None
+        self.encoding = ""
+
+# the total ordering decorator defines the other comparators if these two are defined.
+    def __eq__(self, other):
+         return self.freq == other.freq
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+    def print_node(self):
+        print("Node with letter {} frequency {} and encoding {}".format(self.letter, self.freq, self.encoding))
+
+# depth-first traverse the tree, creating the binary huffman strings for each node of the tree.
+def tree_dfs(node, curstr, leaves):
+    if node is None:
+        return # we shouldn't get here unless None is passed into the first call of this.
+    elif node.right is None and node.left is None:
+        node.encoding = curstr
+        leaves.append(node)
+    else:
+        if node.right is not None:
+            tree_dfs(node.right, curstr + "1", leaves)
+        if node.left is not None:
+            tree_dfs(node.left, curstr + "0", leaves)
+
+# a sanity check that I used while debugging to make sure that each letter has the right encoding, and there are no duplicate encodings.
+def sanity(node):
+    if node.left is None and node.right is None:
+        print("encoding: {} for letter {} and frequency {}".format(int(node.encoding, 2), node.letter, node.freq))
+    else:
+        if node.left:
+            sanity(node.left)
+        if node.right:
+            sanity(node.right)
+
+def make_dict(data):
+    """Makes a char: frequency dict from a string
+    Params: data - str
+    Returns: freq_dict: { char: int }
+    """
+    freq_dict = defaultdict(int)
+    for char in data:
+        freq_dict[char]+=1
+    return freq_dict
+
+def encode(file):
+    """Encodes file into a smaller representation using huffman encoding.
+    Params: file - str
+    Returns: compressed - str"""
+    try:
+        s = open(file).read()
+    except IOError:
+        print("Error opening file: {}".format(file))
+        exit(1)
+
+    # build dict
+    freqs = make_dict(s)
+    minheap = []
+    for k, v in freqs.items():
+        heappush(minheap, TreeNode(k, v))
+
+    # ASSUMPTION: since the bitmap is of size 100 x 100 it must be separated by newlines \n,
+    # so there are at least 2 distinct characters in any input file.
+    parent_node = None # save the current parent node
+
+    while True:
+        node1, node2 = heappop(minheap), heappop(minheap)
+        parent_node = TreeNode(node1.letter + node2.letter, node1.freq + node2.freq)
+        parent_node.left = node1
+        parent_node.right = node2
+        if parent_node.freq == len(s):
+            break
+        else:
+            heappush(minheap, parent_node)
+
+    # populate the tree
+    leaves = []
+    tree_dfs(parent_node, "", leaves)
+    encoding_to_char = {}
+    for leaf in leaves:
+        assert len(leaf.letter) == 1
+        assert leaf.encoding not in encoding_to_char # because the encodings should have been unique.
+        encoding_to_char[leaf.encoding] = leaf.letter
+    char_to_encoding = {v: k for k, v in encoding_to_char.items()}
+    # to be able to decode the file, we need to preserve the dict and also write out the number.
+    int_str = "".join([char_to_encoding[char] for char in s])
+    hex_str = hex(int(int_str, 2))
+    # hack to fix in the future
+    add_zero = False
+    bin_str = bin(int(hex_str, 16))[2:]
+    if bin_str != int_str:
+        add_zero = True
+        bin_str  = '0' + bin_str
+
+    try:
+        os.remove('compressed.txt')
+    except OSError:
+        pass
+
+    with open('compressed.txt', 'wb') as file:
+        pickle.dump((hex_str, char_to_encoding, add_zero), file)
+
+    with open('compressed.txt', 'rb') as file:
+        hs, mapping, add_zero = pickle.load(file)
+
+    assert mapping == char_to_encoding, "houston we have a problem"
+    assert bin_str == int_str, "{} {}".format(bin_str, int_str)
+
+def decode(file):
+    with open(file, 'rb') as f:
+        hex_str, char_to_encoding, add_zero = pickle.load(f)
+    bin_str = bin(int(hex_str, 16))[2:]
+    if add_zero:
+        bin_str = '0' + bin_str
+    encoding_to_char = {v : k for k, v in char_to_encoding.items()}
+    original = ""
+    i = 0
+    while i < len(bin_str):
+        j = i + 1
+        while j < len(bin_str) and bin_str[i:j] not in encoding_to_char.keys():
+            j+=1
+        if j >= len(bin_str):
+            break
+        original+=encoding_to_char[bin_str[i:j]]
+        i = j
+    print(original)
+
+    try:
+        os.remove('decoded.txt')
+    except OSError:
+        pass
+    with open('decoded.txt', 'w') as f:
+        f.write(original)
+        f.write("\n") # for convention
+
+######### UNIT TESTS #########
+
+def make_dict_test():
+    """Tests the make_dict() function"""
+    data = "abracadabra"
+    freqs = make_dict(data)
+    assert 'a' in freqs and freqs['a'] == 5, "TEST FAILED: Expected character 'a' to have a frequency of 5"
+    assert 'b' in freqs and freqs['b'] == 2, "TEST FAILED: Expected 'b' to have a frequency of 2"
+    assert 'r' in freqs and freqs['r'] == 2, "TEST FAILED: Expected 'r' to have a frequency of 2"
+    assert 'c' in freqs and freqs['c'] == 1, "TEST FAILED: Expected 'c' to have a frequency of 2"
+
+def run_tests():
+    """Runs all of the tests."""
+    make_dict_test()
+    print("All tests passed")
+    exit(0)
+
+###### MAIN FUNCTION ########
+
+if __name__ == '__main__':
+    # parse args
+    if len(sys.argv) < 2:
+        print(USAGE_STR)
+        exit(1)
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] != "test":
+            print(USAGE_STR)
+            exit(1)
+        else:
+            run_tests()
+
+    if len(sys.argv) != 3:
+        print(USAGE_STR)
+        exit(1)
+    else:
+        if(sys.argv[1] == "encode"):
+            encode(sys.argv[2]) # encode the file
+        elif(sys.argv[1] == "decode"):
+            decode(sys.argv[2]) # decode the file
+        else:
+            print(USAGE_STR)
+            exit(1)
